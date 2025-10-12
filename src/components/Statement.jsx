58dcc9d6a0989609cc6dd5fc5fe4b5010b41b718
adam_beecham <adam.beecham@gmail.com>
2025-10-12 22:11:40 +0100

    tidy up

--- src/components/Statement.jsx
+++ src/components/Statement.jsx
@@ -47,31 +47,6 @@ export default function Statement({ post, index, velocity = 0, ...rest }) {
 		return () => ctx.revert();
 	}, [index]);
 
-	/*
-	useEffect(() => {
-		if (!el.current) return;
-
-		const skew = Math.max(Math.min(velocity * 2, 40), -40);
-
-		gsap.to(el.current, {
-			skewX: skew,
-			transformOrigin: "center center",
-			duration: 0.3,
-			ease: "power2.out",
-			// markers: true,
-		});
-
-		if (Math.abs(velocity) < 0.05) {
-			gsap.to(el.current, {
-				skewX: 0,
-				duration: 0.6,
-				ease: "power3.out",
-				// markers: true,
-			});
-		}
-	}, [velocity]);
-	*/
-
 	return (
 		<div className="statement" data-index={index} {...rest}>
 			<div className="translate">
