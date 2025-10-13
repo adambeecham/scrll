6c60b4be171d437ab6ad5a7d3198d22e3ab6aa2a
adam_beecham <adam.beecham@gmail.com>
2025-10-13 08:33:17 +0100

    remove infinite on mobile

--- src/hooks/useLenis.js
+++ src/hooks/useLenis.js
@@ -2,16 +2,22 @@ import { useEffect, useRef, useState } from "react";
 import Lenis from "@studio-freight/lenis";
 
 export default function useLenis() {
+
 	const lenisRef = useRef(null);
 	const [ready, setReady] = useState(false);
 
 	useEffect(() => {
+
+		const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
 		const lenis = new Lenis({
 			duration: 2,
 			smooth: true,
-			infinite: true,
+			infinite: !isMobile,
 		});
+
 		lenisRef.current = lenis;
+
 		setReady(true);
 
 		function raf(time) {
@@ -21,6 +27,7 @@ export default function useLenis() {
 		requestAnimationFrame(raf);
 
 		return () => lenis.destroy();
+
 	}, []);
 
 	return { lenisRef, ready };
